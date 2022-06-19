85eae82aa5efc777ce72605154cb4688cb095710
yoichiozaki <yoichiozaki@LAPTOP-5VNER3SA>
2022-06-19 13:18:39 +0900

    プロジェクト ファイルを追加します。

---
+++ ContosoCraftsWebsite/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ContosoCraftsWebsite.Models;
+using ContosoCraftsWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCraftsWebsite.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) =>
+            ProductService = productService;
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            if (request?.ProductId == null)
+                return BadRequest();
+
+            ProductService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string? ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
---
+++ ContosoCraftsWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ContosoCraftsWebsite.Models;
+using ContosoCraftsWebsite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCraftsWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product>? Products { get; private set; }
+
+        public void OnGet() => Products = ProductService.GetProducts();
+    }
+}
